b32034705b149d737536cf55f4ffe8e8aaf159d4
madmardigan53 <madmardigan53@05764da2-f6f5-11dd-9d1d-173cc1d06e3c>
2009-03-20 21:59:30 +0000

    Deleted files for reasons beyond my understanding.

--- branches/modules-to-configurations/src/directive.h
+++
@@ -1,8 +0,0 @@
-#ifndef DIRECTIVE_H
-#define DIRECTIVE_H
-
-typedef enum {
-  LEFT, RIGHT, FORWARD, BACKWARD, UP, DOWN, ACTIVATE, DEACTIVATE, STABILIZE
-} directive;
-
-#endif
--- branches/modules-to-configurations/src/util.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTIL_H
-#define UTIL_H
-
-inline int max (int x, int y)
-{
-  return x > y ? x : y ;
-}
-
-#endif
